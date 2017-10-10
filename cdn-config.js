4cfc6751a2b11873e74c7abebd2a7763b2644ee4
zhang740 <zhang740@vmzclub.com>
2017-10-10 16:38:42 +0800

    up

--- cdn-config.js
+++ cdn-config.js
@@ -1,29 +1,52 @@
+const fs = require('fs');
+const path = require('path');
+
+const projectDir = path.join(__dirname, './');
+
+const baseDir = path.join(projectDir, './');
+const filelist = [];
+
+let dir = `${baseDir}`;
+fs.readdirSync(dir)
+  .filter(file => path.parse(file).ext === '.html')
+  .forEach((doc) => {
+    filelist.push(path.join(dir, doc));
+  });
+
+dir = `${baseDir}/docs/pattern`;
+fs.readdirSync(dir)
+  .filter(file => path.parse(file).ext === '.html')
+  .forEach((doc) => {
+    filelist.push(path.join(dir, doc));
+  });
+
+dir = `${baseDir}/docs/react`;
+fs.readdirSync(dir)
+  .filter(file => path.parse(file).ext === '.html')
+  .forEach((doc) => {
+    filelist.push(path.join(dir, doc));
+  });
+
 module.exports = {
+  cwd: projectDir,
   secret: 'mXvdTx0LJjKDCb2Wqsq5jKpiMEvk2SL1ocVESHCfn5iHb4RWOB+34ihM2e+Rc6SZssxX1vQ041Q2OIg+j6uy7/mrTp/TvDOqORHzLGtLnSA=',
   fileList: [
     {
-      template: [
-        './index.html',
-        './index-cn.html',
-        './changelog.html',
-        './changelog-cn.html',
-        './changelog-cn-cn.html',
-        './404.html',
-      ],
+      template: filelist,
       data: [
         {
           regex: /"\/index.css"/g,
-          file: './index.css',
+          file: `${baseDir}/index.css`,
           template: '"{url}"',
         },
         {
           regex: /"\/index.js"/g,
-          file: './index.js',
+          file: `${baseDir}/index.js`,
           template: '"{url}"',
         },
         {
           regex: /"\/common.js"/g,
-          file: './common.js',
+          file: `${baseDir}/common.js`,
           template: '"{url}"',
         },
       ],
@@ -33,21 +56,21 @@ module.exports = {
     },
     {
       template: [
-        './kitchen-sink/index.html',
-        './kitchen-sink/404.html',
+        `${baseDir}/kitchen-sink/index.html`,
+        `${baseDir}/kitchen-sink/404.html`,
       ],
       data: [
         {
           regex: '/kitchen-sink/kitchen-sink.css',
-          file: './kitchen-sink/kitchen-sink.css',
+          file: `${baseDir}/kitchen-sink/kitchen-sink.css`,
         },
         {
           regex: '/kitchen-sink/common.js',
-          file: './kitchen-sink/common.js',
+          file: `${baseDir}/kitchen-sink/common.js`,
         },
         {
           regex: '/kitchen-sink/kitchen-sink.js',
-          file: './kitchen-sink/kitchen-sink.js',
+          file: `${baseDir}/kitchen-sink/kitchen-sink.js`,
         },
       ],
       options: {
